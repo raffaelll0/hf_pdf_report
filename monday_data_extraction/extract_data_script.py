a67a54169d7193f69f06234ff89e5d02f55d5ef6
raffaele <dev03@ormalab.it>
2023-11-07 12:58:31 +0100

    Initial commit

---
+++ monday_data_extraction/extract_data_script.py
@@ -0,0 +1,284 @@
+import requests
+
+
+apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE0NTc5NTQ4MywiYWFpIjoxMSwidWlkIjoyNzk4NzQzMywiaWFkIjoiMjAyMi0wMi0xNFQwODoyOTo0NC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTExOTUwMTIsInJnbiI6InVzZTEifQ.j052k96lwfIBOtLGWng2xmZul4c_rWnguMOTduJ95DM"
+apiUrl = "https://api.monday.com/v2"
+
+#CI SONO DUE HEADERS POICHE' headers SPECIFICA L'ULTIMA VERSIONE, CHE SERVE PER DETERMINATE QUERY'
+#QUESTO PROBLEMA VA RISOLTO INFATTI ANCHE LA FUNZIONE extract_prev_acc_anno() DEVE UTILIZZARE headers e non headers_old
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': apiKey,
+    'API-Version': '2023-10'
+
+}
+headers_old = {
+    'Authorization': apiKey
+
+}
+
+
+
+def extract_prev_acc_anno():
+    """
+    Questa funzione estrae i dati da una board di monday.com,
+    i dati estrapolati nello specifico sono i numero di preventivi accettati in un anno,
+    viene fatto un controllo tramite query, dove vengono definite due date (inizio-fine)
+
+    la funzione deve essere revisionata poichè non sono ancora state messe le date
+
+    una volta estrapolati i dati essi verranno usati nel file pdf_gen
+    i dati più complicati (per grafici) verranno estrapolati da un'altra funzione
+
+    Args:
+        data: parsed json of the data that the webhook gives
+        challenge: is in a dictionary and contains a value(int)
+
+    Returns:
+        number_of_ids
+    """
+    # CODICE DELLA BOARD
+    id_board_commessa = '2286362496'
+
+    query = ''' { boards(ids: 2286362496) { groups(ids: "topics") { items (limit: 20000) { id } } } } '''
+    data = {'query': query}
+
+    # FACCIAMO UNA RICHIESTA JSON
+    r = requests.post(url=apiUrl, json=data, headers=headers_old)
+
+    # DEFINIAMO IL NOSTRO JSON CON LA VARIABILE response_data
+    response_data = r.json()
+
+    #INDICA IL NUMERO DI ID NEL GRUPPO
+    id_list = [item['id'] for item in response_data['data']['boards'][0]['groups'][0]['items']]
+
+    # Count the number of 'id' entries
+    number_of_ids = len(id_list)
+
+
+    return number_of_ids
+
+
+
+def extract_prev_acc_mese():
+    """
+        Questa funzione estrae i dati da una board di monday.com,
+        i dati estrapolati nello specifico sono i numero di preventivi accettati in un mese,
+        viene fatto un controllo tramite query, dove vengono definite due date (inizio mese-fine mese)
+
+        una volta fatto ciò viene fatto un controllo per vedere quanti items sono presenti, quindi poi
+        questo dato verrà salvato
+
+
+        una volta estrapolati i dati essi verranno usati nel file pdf_gen
+        i dati più complicati (per grafici) verranno estrapolati da un'altra funzione
+
+        Returns:
+            num_items
+        """
+
+    query = '{boards(ids: 2286362496) {items_page(query_params: {rules: [{column_id: "data", compare_value: ["2023-11-01", "2023-11-30"], operator: between}]} ) {items{id name}}}}'
+    data = {'query': query}
+    r = requests.post(url=apiUrl, json=data, headers=headers)
+    response = r.json()
+
+    if 'data' in response and 'boards' in response['data']:
+        items = response['data']['boards'][0]['items_page']['items']
+        num_items = len(items)
+    else:
+        print("No data found in the response.")
+
+    return num_items
+
+def extract_prev_evasi_mese():
+    """
+            Questa funzione estrae i dati da una board di monday.com,
+            i dati estrapolati nello specifico sono i numero di preventivi evasi in un mese,
+            viene fatto un controllo tramite query, dove vengono definite due date (inizio mese-fine mese)
+            stavolta però i dati che ci servono sono filtrati in base ad un altro tipo di colonna
+            ovvero: column_id:"dup__of_data_offerta_contabilt_"
+
+
+            una volta fatto ciò viene fatto un controllo per vedere quanti items sono presenti, quindi poi
+            questo dato verrà salvato
+
+
+            una volta estrapolati i dati essi verranno usati nel file pdf_gen
+            i dati più complicati (per grafici) verranno estrapolati da un'altra funzione
+
+            Args:
+                data: parsed json of the data that the webhook gives
+                challenge: is in a dictionary and contains a value(int)
+
+            Returns:
+                num_items
+            """
+    query = '{boards(ids: 2286362496) {items_page(query_params: {rules: [{column_id: "dup__of_data_offerta_contabilt_", compare_value: ["2023-11-01", "2023-11-30"], operator: between}]} ) {items{id name}}}}'
+
+    data = {'query': query}
+    r = requests.post(url=apiUrl, json=data, headers=headers)
+    response = r.json()
+
+    if 'data' in response and 'boards' in response['data']:
+        items = response['data']['boards'][0]['items_page']['items']
+        num_items = len(items)
+    else:
+        print("No data found in the response.")
+
+    return num_items
+
+
+def extract_prev_acc_consuntivo():
+    """
+            Questa funzione estrae i dati da una board di monday.com,
+            i dati estrapolati nello specifico sono i numero di preventivi accettati in consuntivo,
+            viene fatto un controllo tramite query, dove vengono definite due date (inizio mese-fine mese)
+            stavolta però i dati che ci servono sono filtrati in base ad un altro tipo di colonna e anche in
+            un altro gruppo
+            ovvero: column_id:"dup__of_data_offerta_contabilt_"
+                    groups(ids: "nuovo_gruppo89357")
+
+
+            una volta fatto ciò viene fatto un controllo per vedere quanti items sono presenti, quindi poi
+            questo dato verrà salvato
+
+
+            una volta estrapolati i dati essi verranno usati nel file pdf_gen
+            i dati più complicati (per grafici) verranno estrapolati da un'altra funzione
+
+            Args:
+                data: parsed json of the data that the webhook gives
+                challenge: is in a dictionary and contains a value(int)
+
+            Returns:
+                num_items
+            """
+
+    query = '{ boards(ids: 2286362496) { groups(ids: "nuovo_gruppo89357"){ items_page( query_params: {rules: [{column_id: "data", compare_value: ["2023-11-01", "2023-11-30"], operator: between}]} ) { items { id name } } } } }'
+
+    data = {'query': query}
+    r = requests.post(url=apiUrl, json=data, headers=headers)
+    response = r.json()
+
+    if 'data' in response and 'boards' in response['data'] and 'groups' in response:
+        items = response['data']['boards'][0]['groups']['items_page']['items']
+        num_items = len(items)
+    else:
+        num_items = 0
+
+
+
+    return num_items
+
+
+
+def extract_prev_acc_tot():
+    """
+            Questa funzione estrae i dati da una board di monday.com,
+            i dati estrapolati nello specifico sono il numero tot di preventivi accettati in un anno,
+            viengono fatti due controlli tramite query, dove vengono definite due date (inizio anno-fine anno)
+            stavolta però i dati che ci servono sono filtrati in base ad un altro tipo di colonna e anche in
+            un altro gruppo
+            ovvero per la prima query: column_id:"data"
+                                        groups(ids: "nuovo_gruppo10114")
+
+            ovvero per la seconda query: column_id:"data"
+                                         groups(ids: "nuovo_gruppo89357")
+
+
+            una volta fatto ciò viene fatto un controllo per vedere quanti items sono presenti in entrambe le query,
+            essi verranno addizionati tra di loro e salvati un una variabile
+
+
+            una volta estrapolati i dati essi verranno usati nel file pdf_gen
+            i dati più complicati (per grafici) verranno estrapolati da un'altra funzione
+
+            !IL CODICE DI QUESTA FUNZIONE E' DA REVISIONARE POICHE'
+            !I DATI RISULTANO MAGGIORI DI QUANTO DOVREBBERO ESSERE
+
+            Args:
+                data: parsed json of the data that the webhook gives
+                challenge: is in a dictionary and contains a value(int)
+
+            Returns:
+                tot
+            """
+
+    query = '{ boards(ids: 2286362496) { groups(ids: "nuovo_gruppo10114" ) { items_page(limit:500 query_params: {rules: [{column_id: "data", compare_value: ["2023-01-01", "2023-11-06"], operator: between}]} ) { items { id  } } } } }'
+
+    data = {'query': query}
+    r = requests.post(url=apiUrl, json=data, headers=headers)
+    response = r.json()
+
+    # INDICA IL NUMERO DI ID NEL GRUPPO
+    id_list = [item['id'] for item in response['data']['boards'][0]['groups'][0]['items_page']['items']]
+
+    # Count the number of 'id' entries
+    number_of_ids = len(id_list)
+    print(number_of_ids)
+#####################################################################
+    #QUERY PER LA SCHEDA A CONSUNTIVO
+
+    query_consuntivo = '{ boards(ids: 2286362496) { groups(ids: "nuovo_gruppo89357" ) { items_page(limit:500 query_params: {rules: [{column_id: "data", compare_value: ["2023-01-01", "2023-11-06"], operator: between}]} ) { items { id  } } } } }'
+
+    data_consuntivo = {'query': query_consuntivo}
+    r_consuntivo = requests.post(url=apiUrl, json=data_consuntivo, headers=headers)
+    response_consuntivo = r_consuntivo.json()
+
+    # INDICA IL NUMERO DI ID NEL GRUPPO
+    id_list_consuntivo = [item['id'] for item in response_consuntivo['data']['boards'][0]['groups'][0]['items_page']['items']]
+
+    # Count the number of 'id' entries
+    number_of_ids_consuntivo = len(id_list_consuntivo)
+    print(number_of_ids_consuntivo)
+
+
+    tot = number_of_ids + number_of_ids_consuntivo
+    print(tot)
+
+
+    return tot
+extract_prev_acc_tot()
+
+
+def data_extractor_graphs_pag_2():
+    """
+    questa funzione prenderà i dati da monday.com, essi verranno
+    poi utilizzati per creare dei grafici tramite altair e la libreria pandas
+    i grafici di questa funzione si troveranno nella seconda pagina del pdf
+
+    Args:
+
+    Returns:
+
+    """
+
+
+
+def data_extractor_graphs_pag_3():
+    """
+    questa funzione prenderà i dati da monday.com, essi verranno
+    poi utilizzati per creare dei grafici tramite altair e la libreria pandas
+    i grafici di questa funzione si troveranno nella terza pagina del pdf
+
+    Args:
+
+    Returns:
+
+    """
+
+    #prendo dati da monday.com (probabilmente lista json)
+
+
+    #lo parso
+
+
+    #li definisco e li salvo
+
+
+    #creo dei grafici semplici all'inizio
+
+
+    #una volta capito il meccanismo creo i grafici finali
+
+
---
+++ pdf_gen.py
@@ -0,0 +1,360 @@
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import letter
+
+
+
+from monday_data_extraction.extract_data_script import *
+
+# Create a function to generate the PDF
+def generate_pdf():
+    """
+    Questa funzione crea un pdf basato da griglie rettangolari con
+    all' interno i dati estrapolati da varie funzioni presenti nel file extract_data_script
+    (Per esempio la parte iniziale aggiunge delle descrizioni con delle box sotto)
+
+    il file creato si chiama kpi_report.pdf e si trova all'interno della root di progetto
+
+    Args:
+
+    Returns:
+        pdf file
+    """
+
+    #PRIMA PAGINA
+
+    # Create a SimpleDocTemplate object
+    doc = SimpleDocTemplate("kpi_report.pdf", pagesize=letter)
+
+
+    # Create a list to hold the PDF content
+    story = []
+
+    # Define the title and add it to the story
+    title = "REPORT KPI MESE 2023"
+    title_style = getSampleStyleSheet()["Title"]
+    title_paragraph = Paragraph(title, title_style)
+    story.append(title_paragraph)
+
+    # Add some space below the title
+    story.append(Spacer(1, 12))
+
+    # Author and Date Information
+    author_info = "Autore del report: Christian Trocino<br/>Dati aggiornati al XX/XX/XXXX"
+    author_style = getSampleStyleSheet()["Normal"]
+    author_paragraph = Paragraph(author_info, author_style)
+    story.append(author_paragraph)
+
+    # Add some space below the author information
+    story.append(Spacer(1, 12))
+
+
+########################################################################################################################
+
+
+    # Add the title "PERIODO DI RIFERIMENTO MESE 2023" in the center
+    period_title = "PERIODO DI RIFERIMENTO MESE 2023"
+    period_title_style = getSampleStyleSheet()["Title"]
+    period_title_paragraph = Paragraph(period_title, period_title_style)
+    story.append(period_title_paragraph)
+
+    # Add some space below the title
+    story.append(Spacer(1, 12))
+
+
+    # Create descriptions for each box
+    description1 = "N tot prev.Evasi"
+    description2 = "N tot prev.Accettati"
+    description3 = "N tot prev.acc.consuntivo"
+    description4 = "Importo tot prev.Evasi"
+
+    prev_evasi_mes = extract_prev_evasi_mese()
+    prev_acc_mes = extract_prev_acc_mese()
+    prev_acc_consuntivo = extract_prev_acc_consuntivo()
+
+    # Create a table with four squares containing descriptions and values
+    data = [
+        [description1, description2, description3, description4],
+        [str(prev_evasi_mes), str(prev_acc_mes), str(prev_acc_consuntivo), "4"]
+    ]
+
+    table_data = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data]
+    table = Table(table_data, colWidths=100, rowHeights=50)  # Decreased row heights
+
+    # Apply table styles for a white background with black outer lines
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    story.append(table)
+
+    # Add some space on top of the descriptions
+    story.append(Spacer(1, 12))
+
+
+########################################################################################################################
+
+
+    # Add the title "PERIODO DI RIFERIMENTO MESE 2023" in the center
+    year_title = "PERIODO DI RIFERIMENTO: ANNO 2023"
+    year_title_style = getSampleStyleSheet()["Title"]
+    year_title_paragraph = Paragraph(year_title, year_title_style)
+    story.append(year_title_paragraph)
+
+    # Add some space below the title
+    story.append(Spacer(1, 12))
+
+    # PRENDI I VALORI FETCHATI DA MONDAY NELLO SCRIPT
+    number_of_ids = extract_prev_acc_anno()
+
+    # Create a table with two boxes containing values "1" on the left and "2" on the right
+    data2 = [
+        ["N. Tot.Prev.Accettati", "Importo Tot.Prev.Accettati"],
+        [str(number_of_ids), "2"]
+    ]
+
+    table_data2 = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data2]
+    table2 = Table(table_data2, colWidths=100, rowHeights=50)  # Decreased row heights
+
+    table2.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+
+    story.append(table2)
+
+    # Add some space below the titles
+    story.append(Spacer(1, 12))
+
+
+########################################################################################################################
+
+
+    # Add the descriptions "FATTURATO AD OGGI," "FATTURATO DA EMETTERE," and "FATTURATO PREVISTO 2023"
+    # under all the existing boxes
+    description_left = "FATTURATO AD OGGI"
+    description_center = "FATTURATO DA EMETTERE"
+    description_right = "FATTURATO PREVISTO 2023"
+
+    # Create a table with three boxes containing descriptions and values
+    data3 = [
+        [description_left, description_center, description_right],
+        ["1", "2", "3"]
+    ]
+
+    table_data3 = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data3]
+    table3 = Table(table_data3, colWidths=100, rowHeights=50)  # Decreased row heights
+
+    table3.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    story.append(table3)
+
+
+########################################################################################################################
+
+
+    # Add a page break to start a new page
+    story.append(PageBreak())
+
+    # Create a title for the second page
+    second_page_title = "ANALISI OPERATIVA PROGETTI"
+    second_page_title_style = getSampleStyleSheet()["Title"]
+    second_page_title_paragraph = Paragraph(second_page_title, second_page_title_style)
+    story.append(second_page_title_paragraph)
+
+    # Add some space below the second page title
+    story.append(Spacer(1, 12))
+
+    # Create a table with a single cell containing the number "4"
+    data4 = [
+        ["4"]
+    ]
+
+    table_data4 = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data4]
+    table4 = Table(table_data4, colWidths=500, rowHeights=100)  # Adjust colWidths and rowHeights as needed
+
+    table4.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    story.append(table4)
+
+    # Add some space below the second page title
+    story.append(Spacer(1, 12))
+
+    # Add two more tables with data "5" and "6"
+    data5 = [
+        ["5"]
+    ]
+
+    table_data5 = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data5]
+    table5 = Table(table_data5, colWidths=[500], rowHeights=[150])
+
+    table5.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    story.append(table5)
+
+    # Add some space below the second page title
+    story.append(Spacer(1, 12))
+
+    data6 = [
+        ["6"]
+    ]
+
+    table_data6 = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data6]
+    table6 = Table(table_data6, colWidths=[500], rowHeights=[200])
+
+    table6.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    story.append(table6)
+
+
+########################################################################################################################
+    #SECONDA PAGINA
+
+
+    # Add a page break to start a new page
+    story.append(PageBreak())
+
+    # Create a title for the second page
+    third_page_title = "CONTROLLO DI GESTIONE"
+    third_page_title_style = getSampleStyleSheet()["Title"]
+    third_page_title_paragraph = Paragraph(third_page_title, third_page_title_style)
+    story.append(third_page_title_paragraph)
+
+    # Add some space below the second page title
+    story.append(Spacer(1, 12))
+
+    # Add four more tables with data "5," "6," "7," and "8"
+    for data_value in ["5", "6", "7", "8"]:
+        data = [
+            [data_value]
+        ]
+
+        table_data = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data]
+        table = Table(table_data, colWidths=[400], rowHeights=[100])
+
+        # Add some space below the second page title
+        story.append(Spacer(1, 12))
+
+        table.setStyle(TableStyle([
+            ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+            ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ('SIZE', (0, 0), (-1, -1), 36),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ]))
+
+        story.append(table)
+
+
+########################################################################################################################
+    #TERZA PAGINA
+
+
+    # Add a page break to start a new page
+    story.append(PageBreak())
+
+    # Add three more tables with data "9," "10," and "11"
+    for data_value in ["9", "10", "11"]:
+        data = [
+            [data_value]
+        ]
+
+        table_data = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data]
+        table = Table(table_data, colWidths=[400], rowHeights=[100])
+
+        # Add some space below the second page title
+        story.append(Spacer(1, 12))
+
+        table.setStyle(TableStyle([
+            ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+            ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ('SIZE', (0, 0), (-1, -1), 36),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ]))
+
+        story.append(table)
+
+
+    # Add some space below the fourth page title
+    story.append(Spacer(1, 12))
+
+    # Add the "OBIETTIVI 2023" and "COMMENTI" boxes
+    data_objectives_comments = [
+        ["OBIETTIVI 2023", "COMMENTI"],
+        ["XXX", "XXX"]
+    ]
+
+    table_data_objectives_comments = [[Paragraph(cell, getSampleStyleSheet()["Normal"]) for cell in row] for row in data_objectives_comments]
+    table_objectives_comments = Table(table_data_objectives_comments, colWidths=[300, 300], rowHeights=[50, 50])
+
+    table_objectives_comments.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, -1), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('SIZE', (0, 0), (-1, -1), 36),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    story.append(table_objectives_comments)
+
+
+########################################################################################################################
+
+    # Build the PDF document
+    doc.build(story)
+
+if __name__ == "__main__":
+    generate_pdf()
+
+
+#una volta fatto inizio ad inserire i dati che estrapolo dalle funzioni
+
+
+#implemento i grafici(anche di prova)
+
+
+#aggiungo header loghi dell'azienda (fase finale)
+
